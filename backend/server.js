98a1adb41624dbeed24237fc682fa04173b1ffaa
pruthviraj0106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 <pruthviraj0106@gmail.com>
2025-06-04 14:21:22 +0530

    backend fron link update

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ const upload = multer({
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://splendid-smakager-5467ad.netlify.app/'
+    'https://adultplatform-1.onrender.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
