f111d6a0206926192de6d587a16e9204a6458f82
pruthviraj0106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 <pruthviraj0106@gmail.com>
2025-06-04 12:24:50 +0530

    ipdate build

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import config from '../config';
 
+const API_URL = 'https://adultplatform.onrender.com';
+
 const api = axios.create({
-  baseURL: config.API_URL,
+  baseURL: API_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
